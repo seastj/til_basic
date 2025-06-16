435c306f07e70b390cd9cc21db7168d3d50e1598
seastj <dev.seastj@gmail.com>
2025-06-16 15:48:38 +0900

    [docs] 기본 css 적용완료

--- js/tour.js
+++ js/tour.js
@@ -153,7 +153,7 @@ window.addEventListener("load", function () {
 
   swTourPos.innerHTML = htmlTag;
   new Swiper(".sw_tour", {
-    slidesPerView: 5,
+    slidesPerView: 3,
     grid: {
       rows: 2,
       fill: "row",
@@ -168,7 +168,7 @@ window.addEventListener("load", function () {
 
     // 반응형
     breakpoints: {
-      1024: {
+      1025: {
         slidesPerView: 2,
         slidesPerGroup: 2,
         spaceBetween: 26,
